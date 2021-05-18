e7ba0837c223132aed81694b384dbccaadb33eda
Андрей <andrey19234@gmail.com>
2021-05-18 18:15:55 +0500

    sql migration correct

--- server/common/database.sql
+++ server/common/database.sql
@@ -1,12 +1,11 @@
+create database chatdb;
 create table Status (
 	id SERIAL PRIMARY KEY,
     name varchar(255)
 );
 create table User_ (
 	id SERIAL PRIMARY KEY,
-	token varchar(255),
-	visit_time timestamp,
-	FOREIGN KEY (role_id) references Role(id)
+	token varchar(255)
 );
 create table Message (
 	id SERIAL PRIMARY KEY,
@@ -18,12 +17,12 @@ create table Message (
 	FOREIGN KEY (status_id) references Status(id),
 	FOREIGN KEY (user_id) references User_(id)
 );
-create table Admin() {
+create table Admin(
     id SERIAL PRIMARY KEY,
     login varchar(32),
     password varchar(32),
     salt varchar(32)
-}
+)
 insert into Status (name) values
 ('Отправлено'),('Доставлено'),('Прочитано');
 insert into message (text, time_, status_id, user_id) values
